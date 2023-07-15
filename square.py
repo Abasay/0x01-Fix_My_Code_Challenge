e0e54f98211a9cbe9e3af7d322ac2738591f1461
Abasay <abdulsalamasheem@gmail.com>
2023-07-15 23:09:12 +0000

    user.py square.py

--- square.py
+++ square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-class square():
-    
+class Square:
+    """A Square Function"""
     width = 0
     height = 0
 
@@ -14,10 +14,12 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def permiterOfMySquare(self):
+        """ perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string of the class """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
@@ -25,4 +27,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiterOfMySquare())
